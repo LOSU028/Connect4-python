2f597275b649612ef9f2e73e6171ef2f47a7c245
LOSU028 <116033206+LOSU028@users.noreply.github.com>
2022-10-17 15:30:16 -0500

    Added most recent changes to main file

--- PF.py
+++ PF.py
@@ -1,4 +1,3 @@
-import os 
 global columna
 global espacios_libres
 global ganadorflag
@@ -22,6 +21,7 @@ flag_nueva = False
 flag_cargar = False
 flag_estadisticas = False
 ganadorflag = False
+total_turnos = 1
 regresar_menu = True
 ganador = False
 espacios_libres = 42
@@ -36,13 +36,10 @@ row7 = 6
 turno_activo = str
 
 save_file = "Save_file_conecta4.txt"
-statistics_file = "Statisitcs_file.txt"
 sf = open(save_file, "a")
 
 
 def instrucciones_menu():
-    global turnos_amarillo
-    global turnos_rojo
     global flag_nueva
     global flag_cargar
     global flag_estadisticas
@@ -57,10 +54,10 @@ def instrucciones_menu():
     global total_turnos
     flag_menu = int
 
-    print("Bienvenido a conecta 4 por favor introduzca alguno de los siguientes numeros: \n"
-          "1.Para crear una nueva partida.\n"
+    print("Bienvenido a conecta 4 por favor introduzca alguno de los siguiemntes numeros: \n"
+          "1.Para cargar una nueva partida.\n"
           "2.Para cargar una partida existente.\n"
-          "3.Para mostrar las estadísticas de la última partida concluida.")
+          "3.Para mostrar las estadísticas de la última partida.")
 
     flag_menu = int(input())
 
@@ -69,9 +66,6 @@ def instrucciones_menu():
         flag_menu = int(input())
 
     if flag_menu == 1:
-        os.system("cls")
-        turnos_amarillo = 0
-        turnos_rojo = 0
         espacios_libres = 42
         flag_nueva = True
         row1 = 6
@@ -81,18 +75,17 @@ def instrucciones_menu():
         row5 = 6
         row6 = 6
         row7 = 6
+        espacios_libres = 42
         total_turnos = 1
     elif flag_menu == 2:
-        os.system("cls")
         flag_cargar = True
     elif flag_menu == 3:
-        os.system("cls")
         flag_estadisticas = True
 
     return flag_nueva, flag_cargar, flag_estadisticas
 
 
-def Crear_tablero(flag, flag2):
+def Crear_tablero(flag, flag2, flag3):
     global Tablero
     global Filas
     global columnas
@@ -104,10 +97,6 @@ def Crear_tablero(flag, flag2):
     global row5
     global row6
     global row7
-    global espacios_libres
-    global total_turnos
-    global turnos_amarillo
-    global turnos_rojo
 
     Filas = 6
     columnas = 7
@@ -134,10 +123,6 @@ def Crear_tablero(flag, flag2):
         row5 = int(lines[6])
         row6 = int(lines[7])
         row7 = int(lines[8])
-        espacios_libres = int(lines[9])
-        total_turnos = int(lines[10])
-        turnos_amarillo = int(lines[11])
-        turnos_rojo = int(lines[12])
         sf.close()
         c = 0
         for i in range(0, Filas):
@@ -149,6 +134,7 @@ def Crear_tablero(flag, flag2):
             for j in range(0, columnas):
                 print("|", Tablero[i][j], end=" ")
             print("|", "\n")
+        flag2 = False
 
         return Tablero, turno_activo
 
@@ -165,9 +151,9 @@ def Imprimir_Tablero(tablero, movimiento, turno):
     global row7
 
     if turno == "AMARILLO":
-        ficha = "\033[1;33;40m O \033[0;37;40m"
+        ficha = "\033[0;33;237m O \033[0;37;0m"
     else:
-        ficha = "\033[1;31;40m O \033[0;37;40m"
+        ficha = "\033[0;31;237m O \033[0;37;0m"
 
     if movimiento == "GUARDAR":
         return tablero
@@ -193,7 +179,7 @@ def Imprimir_Tablero(tablero, movimiento, turno):
         elif movimiento == 7:
             tablero[row7 - 1][6] = ficha
             row7 -= 1
-        os.system("cls")
+
         for i in range(0, 6):
             for j in range(0, 7):
                 print("|", tablero[i][j], end=" ")
@@ -216,12 +202,7 @@ def Decision(tablero, movimiento):
 
     guardarflag = False
 
-    while movimiento != "GUARDAR" and not 1 <= int(movimiento) <= 7:
-        print("Movimiento inválido, por favor escoja un columna en rango 1-7 o escoja GUARDAR: ", end="")
-        movimiento = input()
-
     if movimiento == "GUARDAR":
-        os.system("cls")
         sf = open(save_file, "a")
         sf.truncate(0)
         for i in range(0, Filas):
@@ -244,14 +225,6 @@ def Decision(tablero, movimiento):
         sf.write(str(row6))
         sf.write("\n")
         sf.write(str(row7))
-        sf.write("\n")
-        sf.write(str(espacios_libres))
-        sf.write("\n")
-        sf.write(str(total_turnos))
-        sf.write("\n")
-        sf.write(str(turnos_amarillo))
-        sf.write("\n")
-        sf.write(str(turnos_rojo))
         sf.close()
         guardarflag = True
         columna = "GUARDAR"
@@ -279,6 +252,7 @@ def Decision(tablero, movimiento):
         while row7 <= 0 and int(movimiento) == 7:
             print("Movimiento inválido, por favor escoja una columna con espacios disponibles: ", end="")
             movimiento = int(input())
+
         columna = int(movimiento)
         return columna
 
@@ -287,44 +261,22 @@ def cambiar_Jugador(t):
     global turno_activo
     global total_turnos
     global ultimo_turno
-    global turnos_amarillo
-    global turnos_rojo
 
     t += 1
     if t % 2 == 0:
         turno_activo = "ROJO"
         ultimo_turno = "AMARILLO"
-        turnos_amarillo += 1
     else:
         turno_activo = "AMARILLO"
         ultimo_turno = "ROJO"
-        turnos_rojo += 1
 
     total_turnos = t
     return turno_activo, total_turnos
 
 
-def cargar_estadisticas():
-    global turnos_amarillo
-    global turnos_rojo
-    global total_turnos
-    global ultimo_ganador
-
-    turnos_amarillo = int(linesstf[0])
-    turnos_rojo = int(linesstf[1])
-    total_turnos = int(linesstf[2])
-    ultimo_ganador = linesstf[3].strip("\n")
-
-    print("\nTurnos del jugador amarillo: " + str(turnos_amarillo))
-    print("Turnos del jugador rojo: " + str(turnos_rojo))
-    print("Turnos totales de la partida: " + str(total_turnos))
-    print("Ganador de la partida: " + ultimo_ganador)
-    print("Total de partidas terminadas: " + str(partidas_terminadas) + "\n")
-
-
 def Ganador(tablero):
-    ficha_roja = "\033[1;33;40m O \033[0;37;40m"
-    ficha_amarilla = "\033[1;31;40m O \033[0;37;40m"
+    ficha_roja = "\033[0;31;237m O \033[0;37;0m"
+    ficha_amarilla = "\033[0;33;237m O \033[0;37;0m"
     global ganadorflag
 
     '''Columna'''
@@ -376,84 +328,28 @@ def Ganador(tablero):
                 return ganadorflag
 
 
-stf = open(statistics_file, "a")
-stf.close()
-stf = open(statistics_file, "r+")
-linesstf = stf.readlines()
-if linesstf == []:
-    partidas_terminadas = 0
-else:
-    partidas_terminadas = int(linesstf[4])
-stf.close()
 while regresar_menu:
     ganadorflag = False
     instrucciones_menu()
-    sf = open(save_file, "r+")
-    lines = sf.readlines()
-    if flag_cargar and lines == []:
-        print("No se han guardado partidas todavía.")
-        flag_cargar = False
-        sf.close()
-    else:
-        Crear_tablero(flag_nueva, flag_cargar)
-        if flag_estadisticas:
-            stf = open(statistics_file, "r+")
-            linesstf = stf.readlines()
-            if linesstf == []:
-                print("No hay partidas terminadas registradas.")
-            else:
-                cargar_estadisticas()
-            stf.close()
-            flag_nueva = False
-            flag_cargar = False
-            flag_estadisticas = False
-        else:
-            flag_nueva = False
-            flag_cargar = False
-            flag_estadisticas = False
-            while espacios_libres > 0 and ganadorflag == False:
-                print("Turno del jugador: " + turno_activo + "\n")
-                print("Movimiento (escriba GUARDAR si desea guardar y salir de la partida actual): ", end=" ")
-                movimiento = input()
-                movimiento = movimiento.upper()
-                Decision(Tablero, movimiento)
-                print("\n")
-                Tablero = Imprimir_Tablero(Tablero, columna, turno_activo)
-                if guardarflag:
-                    guardarflag = False
-                    break
-                cambiar_Jugador(total_turnos)
-                Ganador(Tablero)
-            if espacios_libres == 0:
-                partidas_terminadas += 1
-                print("\nEMPATE\n")
-                stf = open(statistics_file, "a")
-                partidas_terminadas += 1
-                stf.truncate(0)
-                stf.write(str(turnos_amarillo))
-                stf.write("\n")
-                stf.write(str(turnos_rojo))
-                stf.write("\n")
-                stf.write(str(total_turnos - 1))
-                stf.write("\n")
-                stf.write("Ninguno, fue un empate")
-                stf.write("\n")
-                stf.write(str(partidas_terminadas))
-                stf.close()
+    Crear_tablero(flag_nueva, flag_cargar, flag_estadisticas)
+    flag_nueva = False
+    flag_cargar = False
+    flag_estadisticas = False
+    while espacios_libres > 0 and ganadorflag == False:
+        print("Turno del jugador: " + turno_activo + "\n")
+        print("Movimiento (escriba GUARDAR si desea guardar y salir de la partida actual): ", end=" ")
+        movimiento = input()
+        Decision(Tablero, movimiento)
+        print("\n")
+        Tablero = Imprimir_Tablero(Tablero, columna, turno_activo)
+        if guardarflag:
+            guardarflag = False
+            break
+        cambiar_Jugador(total_turnos)
+        Ganador(Tablero)
+    if espacios_libres == 0:
+        print("\nEMPATE\n")
 
-            elif ganadorflag:
-                print("Gano el jugador: " + ultimo_turno + "\n")
-                stf = open(statistics_file, "a")
-                partidas_terminadas += 1
-                stf.truncate(0)
-                stf.write(str(turnos_amarillo))
-                stf.write("\n")
-                stf.write(str(turnos_rojo))
-                stf.write("\n")
-                stf.write(str(total_turnos - 1))
-                stf.write("\n")
-                stf.write(ultimo_turno)
-                stf.write("\n")
-                stf.write(str(partidas_terminadas))
-                stf.close()
-                regresar_menu = True
+    elif ganadorflag:
+        print("Gano el jugador: " + ultimo_turno + "\n")
+        regresar_menu = True
